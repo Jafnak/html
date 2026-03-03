57da688931234e4050027d0cb601df6299532e4c
Jafna K <jafnakusman@gmail.com>
2026-03-03 20:31:30 +0530

    js modules

---
+++ Javascript/main.js
@@ -0,0 +1,5 @@
+import {add as sum,sub} from "./math.js"  //named export
+import addition from  "./math.js"
+// sum(5,2)
+addition(4,5)
+console.log(sub(10,2))
---
+++ Javascript/math.js
@@ -0,0 +1,6 @@
+export default function sub(a,b){ //default=>main or important one no need a specific name and also no need {}
+    return a-b
+}
+export  function add(a,b){
+    console.log(a+b)
+}
